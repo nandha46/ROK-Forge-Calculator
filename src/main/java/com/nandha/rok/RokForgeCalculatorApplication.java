a711fd2ed761d26354f7227f2b7a4ce489dbda04
Nandha <nandha.kumar790@gmail.com>
2020-07-11 14:40:06 +0530

    first commit

---
+++ src/main/java/com/nandha/rok/RokForgeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.nandha.rok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokForgeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokForgeCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nandha/rok/RokForgeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandha.rok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RokForgeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
